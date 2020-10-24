233b7ffc2ac0c25b2da4c6a66e051b71a4b2aed6
Jesus Castellanos Paez <jesus.castellanos@solem.cl>
2020-10-24 13:59:15 -0500

    Insert: New exercise
    Signed-off-by: Jesus Castellanos Paez <jesus.castellanos@solem.cl>

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # codesignal.com
 Solve exercises https://app.codesignal.com
-v0.0.0
+
+v0.0.1
--- src/com/codesignal/Main.java
+++ src/com/codesignal/Main.java
@@ -4,16 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		//1).
-		// System.out.println(centuryFromYear(101));
+        // System.out.println(add(101,1));
 		//2).
+		// System.out.println(centuryFromYear(101));
+		//3).
 		// System.out.println(checkPalindrome("aabaa"));
 
 	}
 	//1).
+	public static int add(int param1, int param2) {
+	    return param1+param2;
+	}
+	//2).
 	public static int centuryFromYear(int year) {
 		return year/100+((year%100>0)?1:0);
 	}
-	//2).
+	//3).
 	public static boolean checkPalindrome(String inputString) {
 		boolean isTrue=true;
 		int h=0;
@@ -26,4 +32,5 @@ public class Main {
 		}
 		return isTrue;
 	}
+
 }
